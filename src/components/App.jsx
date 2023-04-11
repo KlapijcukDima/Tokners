fa19ab120558df0244e6c6f43d7c8fd20531cc42
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-11 17:27:01 +0300

    added components to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
 import { Header } from "./Header/Header";
 import { Hero } from "./Main-Page/Hero/Hero";
 import { Info } from "./Main-Page/Info/Info";
+// import { Diferent } from './Main-Page/Different/Different';
+// import { Services } from './Main-Page/Services/Services';
+// import { Coming } from './Main-Page/Coming/Coming';
+// import { Details } from './Main-Page/Details/Details';
+// import { Buy } from './Main-Page/Buy/Buy';
+// import { Footer } from './Footer/Footer';
+
+
+
+
+// import buy from '../data/main-page/buy.json';
+// import coming from '../data/main-page/coming.json';
+// import details from '../data/main-page/details.json';
+// import different from '../data/main-page/different.json';
 import info from '../data/main-page/info.json';
+// import services from '../data/main-page/services.json';
+
 
 export const App = () => {
   return (
@@ -9,6 +25,12 @@ export const App = () => {
       <Header />
       <Hero />
       <Info info={info}/>
+      {/* <Diferent />
+      <Services />
+      <Coming />
+      <Details />
+      <Buy />
+      <Footer /> */}
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,31 +1,31 @@
-import css from './Header.module.scss';
+import scss from './Header.module.scss';
 import { ReactComponent as Logo } from '../../assets/images/svg/logo.svg';
 import { ReactComponent as Menu } from '../../assets/images/svg/burger-menu.svg';
 
 export const Header = () => {
   return (
-    <section className={css.section}>
-    <div className={[css.main__block, css.container].join(' ')}>
-        <div className={css.left__block}>
-          <div className={css.header__block}>
-            <Logo className={css.logo}/>
+    <section className={scss.section}>
+    <div className={[scss.main__block, scss.container].join(' ')}>
+        <div className={scss.left__block}>
+          <div className={scss.header__block}>
+            <Logo className={scss.logo}/>
            </div>
 
-            <div className={css.header__right_block}>
-              <ul className={css.header__list}>
-                <li className={css.header__items}>Our team</li>
-                <li className={css.header__items}>Tokens</li>
-                <li className={css.header__items}>Connect wallet</li>
-                <li className={css.header__items}>Lightpaper</li>
+            <div className={scss.header__right_block}>
+              <ul className={scss.header__list}>
+                <li className={scss.header__items}>Our team</li>
+                <li className={scss.header__items}>Tokens</li>
+                <li className={scss.header__items}>Connect wallet</li>
+                <li className={scss.header__items}>Lightpaper</li>
               </ul>
             </div>
         </div>
 
 
-          <div className={css.header__button}>
-            <button className={css.button}>Sign In</button>
-            <button className={css.button__sing_up}>Sign Up</button>
-            <button type='button'  className={css.header__menu}><Menu/></button>
+          <div className={scss.header__button}>
+            <button className={scss.button}>Sign In</button>
+            <button className={scss.button__sing_up}>Sign Up</button>
+            <button type='button'  className={scss.header__menu}><Menu/></button>
           </div>
     </div>
     </section>
