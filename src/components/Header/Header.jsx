51f266e428be4f255f5b020398c1f5dc24db55d2
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-11 17:42:07 +0300

    created link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,29 +5,50 @@ import { ReactComponent as Menu } from '../../assets/images/svg/burger-menu.svg'
 export const Header = () => {
   return (
     <section className={scss.section}>
-    <div className={[scss.main__block, scss.container].join(' ')}>
+      <div className={[scss.main__block, scss.container].join(' ')}>
         <div className={scss.left__block}>
           <div className={scss.header__block}>
-            <Logo className={scss.logo}/>
-           </div>
+            <Logo className={scss.logo} />
+          </div>
 
-            <div className={scss.header__right_block}>
-              <ul className={scss.header__list}>
-                <li className={scss.header__items}>Our team</li>
-                <li className={scss.header__items}>Tokens</li>
-                <li className={scss.header__items}>Connect wallet</li>
-                <li className={scss.header__items}>Lightpaper</li>
-              </ul>
-            </div>
+          <div className={scss.header__right_block}>
+            <ul className={scss.header__list}>
+              <li className={scss.header__items}>
+                <a href="/" className={scss.header__link}>
+                  Our team
+                </a>
+              </li>
+              <li className={scss.header__items}>
+                <a href="/" className={scss.header__link}>
+                  Tokens
+                </a>
+              </li>
+              <li className={scss.header__items}>
+                <a href="/" className={scss.header__link}>
+                  Connect wallet
+                </a>
+              </li>
+              <li className={scss.header__items}>
+                <a href="/" className={scss.header__link}>
+                  Lightpaper
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-
-          <div className={scss.header__button}>
-            <button className={scss.button}>Sign In</button>
-            <button className={scss.button__sing_up}>Sign Up</button>
-            <button type='button'  className={scss.header__menu}><Menu/></button>
-          </div>
-    </div>
+        <div className={scss.header__button}>
+          <button type="button" className={scss.button}>
+            Sign In
+          </button>
+          <button type="button" className={scss.button__sing_up}>
+            Sign Up
+          </button>
+          <button type="button" className={scss.header__menu}>
+            <Menu />
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
