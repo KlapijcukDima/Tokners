2d6cee739b9fc4902d7d6816310804e030cb8219
KlapijcukDima <98531117+KlapijcukDima@users.noreply.github.com>
2023-04-12 17:31:39 +0300

    added comming section

--- src/components/Main-Page/Comming/Comming.jsx
+++ src/components/Main-Page/Comming/Comming.jsx
@@ -1,10 +1,12 @@
 import scss from './Comming.module.scss';
 import { ReactComponent as Logo } from '../../../assets/images/svg/only_logo.svg';
-// import { ReactComponent as Phone } from '../../../assets/images/png/service_phone.png';
+
+
 
 export const Coming = ({ coming }) => {
   return (
     <div className={[scss.comming, scss.container].join(' ')}>
+
       <div className={scss.comming__child}>
         {coming.map(
           ({
@@ -21,15 +23,19 @@ export const Coming = ({ coming }) => {
           }) => {
             return (
               <div key={title} className={scss.comming__block}>
-                <h2 className={scss.heading}>
-                  <Logo width="30" /> {title}
+                <h2 className={scss.heading}> 
+                <Logo width="38" height="32" className={scss.logo} />{title}
                 </h2>
-                <p className={scss.comming__text}>{paragraph1}</p>
+                <p className={`${scss.comming__text_w} ${scss.green}`}>
+                  {paragraph1}
+                </p>
                 <p className={scss.comming__text}>{paragraph2}</p>
                 <p className={scss.comming__text}>{paragraph3}</p>
-                <p className={scss.comming__text}>{paragraph4}</p>
-                <p className={scss.comming__text}>{paragraph5}</p>
-                <p className={scss.comming__text}>{paragraph6}</p>
+                <p className={`${scss.comming__text} ${scss.green}`}>{paragraph4}</p>
+                <p className={scss.comming__text}>
+                  {paragraph5}
+                </p>
+                <p className={`${scss.comming__text} ${scss.green}`}><span className={scss.labka}>“</span>  "{paragraph6}"</p>
                 <p className={scss.comming__text}>{paragraph7}</p>
                 <p className={scss.comming__text}>{paragraph8}</p>
               </div>
@@ -37,9 +43,16 @@ export const Coming = ({ coming }) => {
           }
         )}
       </div>
-      <div>
-        <img src='https://lh3.googleusercontent.com/bi-FFv65o__QzOJ81VzYY-65XGyJIxMq13SdAt48-raWLTYE2JYFLkdL-dYaSPKdVXp4jZ47AvzjBt2p67ngFPY8j0gmnmeDXUazjTGD' alt="" width="600" height="300" />
+      <div className={scss.comming__img_block}>
+        <img
+          src="https://lh3.googleusercontent.com/bi-FFv65o__QzOJ81VzYY-65XGyJIxMq13SdAt48-raWLTYE2JYFLkdL-dYaSPKdVXp4jZ47AvzjBt2p67ngFPY8j0gmnmeDXUazjTGD"
+          alt=""
+          width="891"
+          height="928"
+        />
       </div>
     </div>
   );
 };
+
+
