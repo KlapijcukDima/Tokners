54f73256f5726207e3f022da1cde54b8167cfb21
Paul <pawel.graur47@gmail.com>
2023-04-12 15:18:53 +0300

    Fixed Footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,12 @@
-import { Header } from "./Header/Header";
-import { Hero } from "./Main-Page/Hero/Hero";
-import { Info } from "./Main-Page/Info/Info";
+import { Header } from './Header/Header';
+import { Hero } from './Main-Page/Hero/Hero';
+import { Info } from './Main-Page/Info/Info';
 // import { Diferent } from './Main-Page/Different/Different';
 // import { Services } from './Main-Page/Services/Services';
 // import { Coming } from './Main-Page/Comming/Comming';
 // import { Details } from './Main-Page/Details/Details';
 // import { Buy } from './Main-Page/Buy/Buy';
-// import { Footer } from './Footer/Footer';
-
-
-
+import { Footer } from './Footer/Footer';
 
 // import buy from '../data/main-page/buy.json';
 // import comming from '../data/main-page/comming.json';
@@ -18,19 +15,18 @@ import { Info } from "./Main-Page/Info/Info";
 import info from '../data/main-page/info.json';
 // import services from '../data/main-page/services.json';
 
-
 export const App = () => {
   return (
     <div>
       <Header />
       <Hero />
-      <Info info={info}/>
+      <Info info={info} />
       {/* <Diferent /> */}
       {/* <Services /> */}
       {/* <Coming coming={comming}/> */}
       {/* <Details /> */}
       {/* <Buy /> */}
-      {/* <Footer />  */}
+      <Footer />
     </div>
   );
 };
