9f64db8fc7aac63633cd64adbc7f72aadc4f2878
Paul <pawel.graur47@gmail.com>
2023-04-13 15:59:11 +0300

    Added style for Services and adaptive

--- src/components/Main-Page/Services/Services.jsx
+++ src/components/Main-Page/Services/Services.jsx
@@ -1,19 +1,64 @@
 import scss from './Services.module.scss';
 
-import { ReactComponent as ServiceQ } from '../../../assets/images/svg/service_q.svg';
+import { ReactComponent as ServiceQGreen } from '../../../assets/images/svg/service_q_green.svg';
+import { ReactComponent as ServiceQBlue } from '../../../assets/images/svg/service_q_blue.svg';
+import { ReactComponent as ServiceQYellow } from '../../../assets/images/svg/service_q_yellow.svg';
+import { ReactComponent as ServiceQRed } from '../../../assets/images/svg/service_q_red.svg';
+
 import { ReactComponent as ServiceDot } from '../../../assets/images/svg/service_dot.svg';
 
 export const Services = ({ services }) => {
   return (
     <section className={scss.services}>
-      <div className={scss.container}>
+      <div className={[scss.container, scss.services__container].join(' ')}>
         {services.map(
           ({ number: itemNumber, year: itemYear, list: itemList }, index) => (
-            <div className={scss.services__container} key={index}>
+            <div className={scss.services__contents} key={index}>
               <div className={scss.services__images}>
-                <ServiceQ />
-                <ServiceDot />
-                <p className={scss.services__text}>{itemYear}</p>
+                <h2 className={scss.services__title}>{itemNumber}</h2>
+                {index % 6 === 0 ? (
+                  <ServiceQGreen />
+                ) : index % 6 === 1 ? (
+                  <ServiceQBlue />
+                ) : index % 6 === 2 ? (
+                  <ServiceQYellow />
+                ) : (
+                  <ServiceQRed />
+                )}
+
+                <div className={scss.services__images__container}>
+                  {index % 6 === 0 ? (
+                    <ServiceDot
+                      className={[
+                        scss.services__images__dot,
+                        scss.services__images__dot__green,
+                      ].join(' ')}
+                    />
+                  ) : index % 6 === 1 ? (
+                    <ServiceDot
+                      className={[
+                        scss.services__images__dot,
+                        scss.services__images__dot__blue,
+                      ].join(' ')}
+                    />
+                  ) : index % 6 === 2 ? (
+                    <ServiceDot
+                      className={[
+                        scss.services__images__dot,
+                        scss.services__images__dot__yellow,
+                      ].join(' ')}
+                    />
+                  ) : (
+                    <ServiceDot
+                      className={[
+                        scss.services__images__dot,
+                        scss.services__images__dot__red,
+                      ].join(' ')}
+                    />
+                  )}
+
+                  <p className={scss.services__text}>{itemYear}</p>
+                </div>
               </div>
 
               <div className={scss.services__contents}>
