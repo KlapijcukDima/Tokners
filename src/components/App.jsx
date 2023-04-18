eccc4553d113f0ab8d480a1664bb9305b7311141
Paul <pawel.graur47@gmail.com>
2023-04-18 13:31:37 +0300

    created new section Details, added style and conected json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ import { Info } from "./Main-Page/Info/Info";
 import { Different } from './Main-Page/Different/Different';
 import { Services } from './Main-Page/Services/Services';
 import { Coming } from './Main-Page/Comming/Comming';
-// import { Details } from './Main-Page/Details/Details';
+import { Details } from './Main-Page/Details/Details';
 // import { Buy } from './Main-Page/Buy/Buy';
 import { Footer } from './Footer/Footer';
 
@@ -13,7 +13,7 @@ import { Footer } from './Footer/Footer';
 
 // import buy from '../data/main-page/buy.json';
 import comming from '../data/main-page/comming.json';
-// import details from '../data/main-page/details.json';
+import details from '../data/main-page/details.json';
 import different from '../data/main-page/different.json';
 import info from '../data/main-page/info.json';
 import services from '../data/main-page/services.json';
@@ -28,7 +28,7 @@ export const App = () => {
       <Different different={different}/>
       <Services services={services}/>
       <Coming coming={comming}/>
-      {/* <Details /> */}
+      <Details details={details}/>
       {/* <Buy /> */}
        <Footer />
     </div>
